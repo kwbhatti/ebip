6300384f3263ec83ab38cc31b2380751a2eba8be
Khurram Bhatti <kbhatti@intrafi.com>
2026-03-18 11:08:49 -0400

    clean up milestone 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pipeline.wf_exe_pipeline import run_wf_exe_pipeline
 from pipeline.wf_logs_pipeline import run_wf_logs_pipeline
+from pipeline.hybrid_scoring_pipeline import run_hybrid_scoring_pipeline
 
 def main():
 
@@ -7,8 +8,9 @@ def main():
 
     # print(wf_exe_df.sort_values("anomaly_score").head(20))
 
-    wf_logs_df = run_wf_logs_pipeline()
+    # wf_logs_df = run_wf_logs_pipeline()
 
+    run_hybrid_scoring_pipeline()
 
 if __name__ == "__main__":
     main()
